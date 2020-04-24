766fddaad0052c52b73dfb98b656731a855926ee
DarrenDV <darrendevre@hotmail.com>
2020-04-24 13:21:26 +0200

    maybe fix

---
+++ Assets/Dice/Scrips/AudioActivate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(KonamiCode))]
+public class AudioActivate : MonoBehaviour
+{
+    private KonamiCode code;
+    public AudioSource Konami;
+    public bool Activated;
+
+    void Awake()
+    {
+        code = GetComponent<KonamiCode>();
+        Konami = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (code.success)
+        {
+            Activated = true;
+            Konami.Play();
+        }
+    }
+}

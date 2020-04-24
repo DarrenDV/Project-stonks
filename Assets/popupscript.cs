702c87e77a4a75327b0d9aca681a26fd75661b56
T. Verheul <tamiraverheul@gmail.com>
2020-04-24 14:33:22 +0200

    mag het echt?

--- Assets/popupscript.cs
+++ Assets/popupscript.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class popupscript : MonoBehaviour
 {
-    int playerCount = 0;
-   public List<string> colors = new List<string>();
+    public int playerCount = 0;
+    public bool rood;
+    public bool blauw;
+    public bool paars;
+    public bool geel;
+    public bool zwart;
+    public bool groen;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     public void setPlayerCount(int playerCount)
@@ -21,29 +26,92 @@ public class popupscript : MonoBehaviour
     {
         return playerCount;
     }
+
     // Update is called once per frame
     void Update()
     {
-        
+    
     }
 
-    public void colorPick(string color) 
+    public void Rood() {
+        if (rood == true) {
+            rood = false;
+            playerCount--;
+        }
+        else
+        {
+            rood = true;
+            playerCount++;
+        }
+    }
+
+    public void Blauw()
     {
-        foreach (var knownColor in colors)
+        if (blauw == true)
         {
-            if (string.Equals(knownColor, color))
-            {
-
-            }
-            else
-            {
-                colors.Add(color);
-            }
+            blauw = false;
+            playerCount--;
         }
-        if (colors.Count == 0)
+        else
         {
-            colors.Add(color);
+            blauw = true;
+            playerCount++;
         }
+    }
 
+    public void Geel()
+    {
+        if (geel == true)
+        {
+            geel = false;
+            playerCount--;
+        }
+        else
+        {
+            geel = true;
+            playerCount++;
+        }
+    }
+
+    public void Paars()
+    {
+        if (paars == true)
+        {
+            paars = false;
+            playerCount--;
+        }
+        else
+        {
+            paars = true;
+            playerCount++;
+        }
+    }
+
+    public void Zwart()
+    {
+        if (zwart == true)
+        {
+            zwart = false;
+            playerCount--;
+        }
+        else
+        {
+            zwart = true;
+            playerCount++;
+        }
+    }
+
+    public void Groen()
+    {
+        if (groen == true)
+        {
+            groen = false;
+            playerCount--;
+        }
+        else
+        {
+            groen = true;
+            playerCount++;
+        }
     }
 }

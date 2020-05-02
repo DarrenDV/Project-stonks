a226e3db266060e66f38b7bdd12575a86a9aec45
jiminy0410 <jiminy0410@gmail.com>
2020-05-02 17:30:21 +0200

    de dobbelstenen doen het in het menutje

--- Assets/scripts/cameracontroller.cs
+++ Assets/scripts/cameracontroller.cs
@@ -18,14 +18,36 @@ public class cameracontroller : MonoBehaviour
     public int startPos = 0;
     public int toMove;
     public int playerCount;
+    public int location;
     public bool SecondDice;
     public bool moving;
 
     public GameObject activator;
     public GameObject cam;
+    public GameObject rotator;
     public GameObject popup;
-    public bool blueDice;
     public GameObject[] knoppies;
+    public GameObject numDice;
+    public GameObject makDice1;
+    public GameObject makDice2;
+
+    public bool blueDice;
+    public bool Blue10;
+    public bool Blue20;
+
+    public bool Windmill;
+    public bool Fish;
+    public bool Flower;
+    public bool Boat;
+    public bool Wheel;
+    public bool Stones;
+
+    public bool Windmill2;
+    public bool Fish2;
+    public bool Flower2;
+    public bool Boat2;
+    public bool Wheel2;
+    public bool Stones2;
     #endregion
 
     #region Serialized Vars
@@ -56,8 +78,25 @@ public class cameracontroller : MonoBehaviour
 
     void LateUpdate()
     {
-
         blueDice = activator.GetComponent<NumberDiceCheckZoneScript>().blueDice;
+        Blue10 = activator.GetComponent<NumberDiceCheckZoneScript>().Blue10;
+        Blue20 = activator.GetComponent<NumberDiceCheckZoneScript>().Blue20;
+
+        Windmill = activator.GetComponent<NumberDiceCheckZoneScript>().Windmill;
+        Fish = activator.GetComponent<NumberDiceCheckZoneScript>().Fish;
+        Flower = activator.GetComponent<NumberDiceCheckZoneScript>().Flower;
+        Boat = activator.GetComponent<NumberDiceCheckZoneScript>().Boat;
+        Wheel = activator.GetComponent<NumberDiceCheckZoneScript>().Wheel;
+        Stones = activator.GetComponent<NumberDiceCheckZoneScript>().Stones;
+
+        Windmill2 = activator.GetComponent<NumberDiceCheckZoneScript>().Windmill2;
+        Fish2 = activator.GetComponent<NumberDiceCheckZoneScript>().Fish2;
+        Flower2 = activator.GetComponent<NumberDiceCheckZoneScript>().Flower2;
+        Boat2 = activator.GetComponent<NumberDiceCheckZoneScript>().Boat2;
+        Wheel2 = activator.GetComponent<NumberDiceCheckZoneScript>().Wheel2;
+        Stones2 = activator.GetComponent<NumberDiceCheckZoneScript>().Stones2;
+
+        location = rotator.GetComponent<CamMove>().location;
 
         for (int i = 0; i < 12; i++)
         {
@@ -113,7 +152,7 @@ public class cameracontroller : MonoBehaviour
 
                 }
             }
-            
+
             if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 1000, layerMask3))
             {
                 if (hit.collider.CompareTag("GameController"))
@@ -122,7 +161,26 @@ public class cameracontroller : MonoBehaviour
                     {
                         if (hit.collider.gameObject == knoppies[i])
                         {
-                            toMove = i+1;
+                            toMove = i + 1;
+                            if (location == 0)
+                            {
+                                numDice.transform.rotation = Quaternion.Euler(60, 270, 0);
+                                makDice1.transform.rotation = Quaternion.Euler(60, 270, 0);
+                                makDice2.transform.rotation = Quaternion.Euler(60, 270, 0);
+                            }
+                            else if (location == 1)
+                            {
+                                numDice.transform.rotation = Quaternion.Euler(60, 0, 0);
+                                makDice1.transform.rotation = Quaternion.Euler(60, 0, 0);
+                                makDice2.transform.rotation = Quaternion.Euler(60, 0, 0);
+                            }
+                            else if (location == -1)
+                            {
+                                numDice.transform.rotation = Quaternion.Euler(60, 180, 0);
+                                makDice1.transform.rotation = Quaternion.Euler(60, 180, 0);
+                                makDice2.transform.rotation = Quaternion.Euler(60, 180, 0);
+                            }
+
                             popup.SetActive(false);
                         }
                     }
@@ -166,6 +224,64 @@ public class cameracontroller : MonoBehaviour
             if (kijk == 120)
             {
                 popup.SetActive(true);
+                if (Blue10)
+                {
+                    numDice.transform.Rotate(270.0f, 180.0f, 0.0f, Space.Self);
+                }
+                else if (Blue20)
+                {
+                    numDice.transform.Rotate(90.0f, 180.0f, 0.0f, Space.Self);
+                }
+
+                if (Windmill)
+                {
+                    makDice1.transform.Rotate(270.0f, 180.0f, 0.0f, Space.Self);
+                }
+                else if (Fish)
+                {
+                    makDice1.transform.Rotate(0.0f, 90.0f, 0.0f, Space.Self);
+                }
+                else if (Flower)
+                {
+                    makDice1.transform.Rotate(180.0f, 180.0f, 0.0f, Space.Self);
+                }
+                else if (Boat)
+                {
+                    makDice1.transform.Rotate(0.0f, 180.0f, 0.0f, Space.Self);
+                }
+                else if (Wheel)
+                {
+                    makDice1.transform.Rotate(0.0f, 90.0f, 180.0f, Space.Self);
+                }
+                else if (Stones)
+                {
+                    makDice1.transform.Rotate(90.0f, 180.0f, 0.0f, Space.Self);
+                }
+
+                if (Windmill2)
+                {
+                    makDice2.transform.Rotate(270.0f, 180.0f, 0.0f, Space.Self);
+                }
+                else if (Fish2)
+                {
+                    makDice2.transform.Rotate(0.0f, 90.0f, 0.0f, Space.Self);
+                }
+                else if (Flower2)
+                {
+                    makDice2.transform.Rotate(180.0f, 180.0f, 0.0f, Space.Self);
+                }
+                else if (Boat2)
+                {
+                    makDice2.transform.Rotate(0.0f, 180.0f, 0.0f, Space.Self);
+                }
+                else if (Wheel2)
+                {
+                    makDice2.transform.Rotate(0.0f, 90.0f, 180.0f, Space.Self);
+                }
+                else if (Stones2)
+                {
+                    makDice2.transform.Rotate(90.0f, 180.0f, 0.0f, Space.Self);
+                }
             }
             kijk++;
         }
@@ -175,5 +291,13 @@ public class cameracontroller : MonoBehaviour
             kijk = 0;
         }
 
+        if (SecondDice)
+        {
+            makDice2.SetActive(true);
+        }
+        else
+        {
+            makDice2.SetActive(false);
+        }
     }
 }

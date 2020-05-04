ed6fed5832601e87eff3950470c6562916287e40
VERMEIT3 <thomas.vermeij2@hva.nl>
2020-05-04 22:20:50 +0200

    Yeet

---
+++ Assets/scripts/Failed scripts/MouseDrag.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseDrag : MonoBehaviour
+{
+    private Vector3 mOffset;
+
+    private float mZcoord;
+    private void OnMouseDown()
+    {
+        mZcoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+        //Store offset = gameObject world pos - mouse world pos
+        mOffset = gameObject.transform.position - GetMouseWorldPos();
+    }
+
+    private Vector3 GetMouseWorldPos()
+    {
+        Vector3 mousePoint = Input.mousePosition;
+
+        mousePoint.z = mZcoord;
+
+        return Camera.main.ScreenToWorldPoint(mousePoint);
+    }
+
+    private void OnMouseDrag()
+    {
+        transform.position = GetMouseWorldPos() + mOffset;
+    }
+}
